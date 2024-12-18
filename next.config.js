3d082bd7ee664c09a4d32da24dc0308802db1d7b
Mateo Algañarás <tutealga@gmail.com>
2024-12-18 20:29:23 -0300

    Add metatitle and metadescription for categories

--- next.config.js
+++ next.config.js
@@ -1,5 +1,55 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+   return [
+    {
+      source: '/congelados',
+      destination: '/preparados',
+      permanent: true,
+    },
+    {
+      source: '/carnes-vacunas',
+      destination: '/cortes-vacunos',
+      permanent: true,
+    },
+    {
+      source: '/cortes-para-parrilla',
+      destination: '/carnes-para-parrilla',
+      permanent: true,
+    },
+    {
+      source: '/cortes-para-plancha',
+      destination: '/carnes-para-plancha',
+      permanent: true,
+    }
+    ,
+    {
+      source: '/cortes-para-cacerola',
+      destination: '/carnes-para-cacerola',
+      permanent: true,
+    },
+    {
+      source: '/cortes-para-horno',
+      destination: '/carnes-para-horno',
+      permanent: true,
+    },
+    {
+      source: '/carnes-de-cerdo',
+      destination: '/cortes-cerdo',
+      permanent: true,
+    },
+    {
+      source: '/cortes-para-milanesas',
+      destination: '/carnes-para-milanesas',
+      permanent: true,
+    },
+    {
+      source: '/carnes-de-pollo',
+      destination: '/cortes-pollo',
+      permanent: true,
+    },
+   ]
+  },
   images: {
     domains: ['firebasestorage.googleapis.com'],
     remotePatterns: [
